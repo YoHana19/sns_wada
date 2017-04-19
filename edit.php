0554f14b79aaab2e61f537ff6031f594f9f40a4f
Kamiya Shogo <kamisho0528.message@gmail.com>
2017-04-19 23:37:07 +0800

    ちょい修正

--- edit.php
+++ edit.php
@@ -25,6 +25,49 @@ $back_picture_path = $_POST['back_picture_path'];
 $self_intro = $_POST['self_intro'];
 }
 
+// 空だった時
+  if ($_POST['nick_name'] == "") {
+    $errors['nick_name'] = 'blank';
+  }
+
+    if ($_POST['email'] == "") {
+    $errors['email'] = 'blank';
+  }
+
+// メモ「上記のメールアドレスと一致するかをif文で出すこと!!!」
+    if ($_POST['email_check'] != $_POST['email']) {
+    $errors['email'] = 'not_match';
+  }
+
+if(empty($errors)){
+// 画像のバリエーション
+  $file_name = $_FILES['user_picture_path']['name'];
+}
+
+if(empty($errors)){
+// 画像のバリエーション
+  $file_name = $_FILES['back_picture_path']['name'];
+}
+
+    // メールアドレスの重複チェック
+    if(empty($errors)){
+      // DBのmembersテーブルに入力されたメールアドレスと同じデータがあるかどうか検索し取得
+      try{
+           $sql = 'SELECT COUNT(*) AS `cnt` FROM `members` WHERE `email` =?'; //COUNT-変数に含まれる全ての要素、あるいはオブジェクトに含まれる何かの数を数える
+           $data =array($email);
+           $stmt = $dbh->prepare($sql);
+           $stmt->execute($data);
+           $record = $stmt->fetch(PDO::FETCH_ASSOC);
+           var_dump($record);
+           if($record['cnt']>0){
+            // 同じメールアドレスがDB内に存在したため
+              $errors['email'] = 'duplicate'; //duplicate-重複
+           }
+
+      }catch(PDOException $e){
+          echo 'SQL文実行時エラー:' . $e->message();
+      }
+    }
 
 
  ?>
@@ -37,29 +80,67 @@ $self_intro = $_POST['self_intro'];
   <meta charset="utf-8">
 </head>
 <form method="POST" action="">
+
+
   <div>
     <label>ニックネーム</label>
     <input type="nick_name" name="nick_name" value="<?php echo $login_member_id['nick_name']; ?>">
+    <?php if(isset($errors['nick_name']) && $errors['nick_name'] == 'blank'): ?> <!-- コロン構文 -->
+      <p style="color:red; font-size:10px; margin-top:2px; ">ニックネームを入力してください</p>
+    <?php endif; ?>
   </div>
+
 
   <div>
     <label>メールアドレス</label>
     <input type="email" name="email" value="<?php echo $login_member_id['email']; ?>">
+        <?php if(isset($errors['email']) && $errors['email'] == 'blank'): ?> <!-- コロン構文 -->
+      <p style="color:red; font-size:10px; margin-top:2px; ">メールアドレスを入力してください</p>
+    <?php endif; ?>
   </div>
+
 
   <div>
     <label>メールアドレス確認用</label>
-    <input type="email_check" name="email_check" value">
+    <input type="email" name="email" value="">
+    <?php if(isset($errors['email']) && $errors['email'] == 'blank' && $errors['email'] != $errors['email']): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">メールアドレスを正しく入力してください</p>
+    <?php endif; ?>
   </div>
+
 
   <div>
     <label>アイコン画像</label>
-    <input type="file" name="picture_path" value="<?php echo $login_member_id['user_picture_path']; ?>">
+    <input type="file" name="user_picture_path" value="<?php echo $login_member_id['user_picture_path']; ?>">
+    <?php if(isset($errors['user_picture_path']) && $errors['user_picture_path'] == 'blank'): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">アイコン画像を選択してください</p>
+    <?php endif; ?>
   </div>
+
+    <?php if(isset($errors['user_picture_path']) && $errors['user_picture_path'] == 'type'): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">アイコン画像は「.gif」,「.jpg」,「.png」の画像を指定してください</p>
+    <?php endif; ?>
+
+    <?php if(!empty($errors)): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">アイコン画像を再度指定してください。</p>
+    <?php endif; ?>
+
 
   <div>
     <label>背景画像</label>
-    <input type="file" name="picture_path" value="<?php echo $login_member_id['back_picture_path']; ?>">
+    <input type="file" name="back_picture_path" value="<?php echo $login_member_id['back_picture_path']; ?>">
+    <?php if(isset($errors['back_picture_path']) && $errors['back_picture_path'] == 'blank'): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">背景画像を選択してください</p>
+    <?php endif; ?>
+
+    <?php if(isset($errors['back_picture_path']) && $errors['back_picture_path'] == 'type'): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">背景画像は「.gif」,「.jpg」,「.png」の画像を指定してください</p>
+    <?php endif; ?>
+
+    <?php if(!empty($errors)): ?>
+      <p style="color:red; font-size:10px; margin-top:2px; ">背景画像を再度指定してください。</p>
+    <?php endif; ?>
+
   </div>
 
 <div>
