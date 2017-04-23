ae3fb987f44b4df3c13a98fec354f377109fba2d
MacUser <fea2gg@gmail.com>
2017-04-23 22:14:11 +0800

    更新

--- left_sidebar.php
+++ left_sidebar.php
@@ -1,177 +1,83 @@
 <?php 
-
-require 'dbconnect.php';
 session_start();
-$_SESSION['id'] = 1;
-// $sql = 'SELECT * FROM `friends` f, `chats` c WHERE f.login_member_id = c.sender_id OR f.login_member_id = c.reciever_id AND f.login_member_id =?';
-// $stmt = $dbh->prepare($sql);
-// $stmt->execute(); //
-// executeで取得したデータはobject型である
-// このままではPHPで扱いづらい
-// echo'<pre>';
-// var_dump($stmt);
-// echo'</pre>';
-$sql = 'SELECT * FROM `chats` WHERE `sender_id` = ? ORDER BY `created` DESC';
-$data = array($_SESSION['id']);
+require ('dbconnect.php');
+
+
+$_SESSION['login_member_id'] = 1;
+
+$sql = 'SELECT * FROM  `members`';
+$data =array($_SESSION['login_member_id']);
 $stmt = $dbh->prepare($sql);
-$stmt->execute($data); //
-$friends =array();
-// $chat = $stmt->fetch(PDO::FETCH_ASSOC);
-while ($chat = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $sql = 'SELECT * FROM `members` WHERE `member_id` = ?';
-    $data1 = array($chat['reciever_id']);
-    $stmt1 = $dbh->prepare($sql);
-    $stmt1->execute($data1);
-    $member = $stmt1->fetch(PDO::FETCH_ASSOC);
+$stmt->execute($data);
+$login_member_id = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $friends[] =array('member_id'=>$member['member_id'],
-                    'nick_name'=>$member['nick_name']);
 
-}
-// var_dump($chat);
-var_dump($friends);
-echo '<br>';
+//roomsテーブルから新着順に10件
 
-$c = count($friends);
-if ($c >= 10) {
-    for ($i=0; $i < 10; $i++) { 
-      $ii = $i + 1;
-        echo '友達'. $ii .'name;' . $friends[$i]['nick_name'] .'<br>';
-    }
-}else{
-    for ($i=0; $i < $c; $i++) {
-    $ii = $i + 1; 
-        echo '友達'. $ii .'name;' . $friends[$i]['nick_name'] .'<br>';
-    }
-}
- ?>
- <!DOCTYPE html>
+$sql = 'SELECT * FROM `rooms` WHERE sender_id = ? ORDER BY modified DESC LIMIT 1, 10';
+        $data1 = array($_SESSION['login_member_id']);
+        $stmt1 = $dbh->prepare($sql);
+        $stmt1->execute($data1);
+        $friends = array();
+        while ($chat = $stmt1->fetch(PDO::FETCH_ASSOC)) {
+                $sql = 'SELECT * FROM `members` WHERE member_id = ?';
+                $data2 = array($chat['reciever_id']);
+                $stmt2 = $dbh->prepare($sql);
+                $stmt2->execute($data2);
+                $m = $stmt2->fetch(PDO::FETCH_ASSOC);
+
+                $friends[]= array('member_id'=>$m['member_id'], 'nick_name'=>$m['nick_name'], 'user_picture_path'=>$m['user_picture_path'], 'self_intro'=>$m['self_intro']);
+                
+              }
+             //  $c = count($friends);
+             //  for ($i=0; $i < $c; $i++) { 
+             //   echo $friends[$i]['nick_name'];
+             //   echo'<br>';
+             //   echo $friends[$i]['user_picture_path'];
+             //   echo'<br>';
+             //   echo $friends[$i]['self_intro'];
+             //   echo'<br>';
+             // }
+
+
+
+
+?>
+
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-<title>左サイドバー</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="../assets/css/left_sidebar.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css"> <!-- bootsnippを使うためには必要 -->
+  <title></title>
 </head>
 <body>
-<!-- 簡易個人プロフ作成 -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <img src="assets/images/saito.jpeg" class="img-responsive img-circle" />
-        <h1>齋藤 由佳</h1>
-        <p>おねぇさん トレンディだね 齋藤さん</p>
-        <div class="clearfix"></div>
 
-        <div class="panel panel-default">  <!-- 「Friends」の一番上の枠 -->
-          <div class="panel-heading c-list">  <!-- 「Friends」の一番上の枠のパネル -->
-            <span class="title">Friend's</span>
-          </div>
 
-          <!-- 直近連絡とった友達順に10件表示 -->
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-            <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span><br/>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-          <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-            <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-            <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-            <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="../assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-            <li class="list-group-item">
-            <div class="col-xs-12 col-sm-6">
-              <img src="../assets/images/wada.jpg" class="img-responsive img-circle" />
-            </div>
-            <div class="col-xs-12 col-sm-10">
-              <span class="name">和田 隆宏</span>
-              <p>わだわだや<br> あぁわだわだや<br> わだわだや</p>
-            </div>
-            <div class="clearfix"></div>
-          </li>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="msg">
+  <img src="assets/images/<?php echo $login_member_id['user_picture_path']; ?>" width="50" height="50">
+  <br>
+  <a href="profile.php"><?php echo $login_member_id['nick_name'];?></a>
+  <p><?php echo $login_member_id['self_intro'];?></p>
+  <p><?php echo $login_member_id['bozu_points'];?>point</p>
+  <p><?php $c = count($friends); ?>
+              <?php for ($i=0; $i < $c; $i++) : ?> 
+              <img src="assets/images/<?php echo $login_member_id['user_picture_path']; ?>" width="50" height="50">
+              <br>
+              <a href="profile.php?<?php $_REQUEST['']; ?>"><?php echo $friends[$i]['nick_name'];?></a>
+              <br>
+               <?php echo $friends[$i]['self_intro'];?>
+               <br>
+              <?php endfor;?>
+
+             </p>
+             <br>
+
+
+</div>
+
+
 </body>
+
 </html>
+
+
