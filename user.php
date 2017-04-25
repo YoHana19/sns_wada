d198ffc782347dd2c0541cc38be7715c989bf051
yohanashima <yohanashima@mail.com>
2017-04-25 21:35:21 +0800

    他人ページの友だち追加処理基本機能は終了

--- user.php
+++ user.php
@@ -2,7 +2,8 @@
 session_start();
 require('dbconnect.php');
 
-$user_id = 3;
+$_SESSION['login_member_id'] = 1;
+$user_id = 5;
 ?>
 
 
@@ -11,16 +12,43 @@ $user_id = 3;
 <head>
   <meta charset="utf-8">
   <title></title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
+
+  <?php
+    // 友達申請済みかどうかの判定処理
+    $sql = 'SELECT * FROM `friends` WHERE `login_member_id`=? AND `friend_member_id`=?';
+    $data = array($_SESSION['login_member_id'],$user_id);
+    $friend_stmt = $dbh->prepare($sql);
+    $friend_stmt->execute($data);
+  ?>
+
   <div>
-    <?php if($is_like = $is_like_stmt->fetch(PDO::FETCH_ASSOC)): ?>
-      <!-- 申請済み -->
-      <input type="submit" value="申請済み" id="<?php echo $user_id; ?>" class="like btn btn-danger btn-xs">
-    <?php else: ?>
+    <?php if($friend = $friend_stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+      <?php if ($friend['state'] == 1) { ?>
+        <!-- 友達 -->
+        <p>友達</p>
+      <?php } else { ?>
+        <!-- 申請済み -->
+        <input type="submit" value="申請取り消し" id="<?php echo $user_id; ?>" class="friend btn btn-danger btn-xs">
+        
+      <?php } ?>
+    <?php } else { ?>
       <!-- 未申請 -->
-      <input type="submit" value="友達申請" id="<?php echo $user_id; ?>" class="like btn btn-primary btn-xs">
-    <?php endif; ?>
+      <input type="submit" value="友達申請" id="<?php echo $user_id; ?>" class="friend btn btn-primary btn-xs">
+    <?php } ?>
+    <!-- 確認メッセージ -->
+    <p id="done_msg" style="display: none">友達リクエストを送りました。</p>
+    <p id="undone_msg" style="display: none">友達リクエストを取り消しました。</p>
   </div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/friend.js"></script>
+
 </body>
 </html>
