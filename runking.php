3a7f0207152f6221e89ce6a86cc17d1b2326dd80
KOSEI INOUE <816kosei2017@gmail.com>
2017-04-25 16:01:49 +0800

    更新 : 一致する値を統合

--- runking.php
+++ runking.php
@@ -4,23 +4,36 @@ require('dbconnect.php');
 
 // ランキングよし・あし、俳人・句それぞれのランキング上位3件を取ってくる
 
-$sql = 'SELECT * FROM `haikus` WHERE  ';
-$data = array();
-$stmt = $dbh->prepare($sql);
-$stmt->execute($data);
+// よしランキング用sql文
+$sql = 'SELECT * FROM `likes`';
+$like_stmt = $dbh->prepare($sql);
+$like_stmt->execute();
 
-$search_word = '';
-if (isset($_GET['search_word']) && !empty($_GET['search_word'])) {
-  // 検索の場合の処理
-  $search_word= $_GET['search_word'];
-  $sql = sprintf('SELECT t.*, m.nick_name, m.picture_path FROM `tweets` AS t LEFT JOIN `members` AS m ON t.member_id=m.member_id WHERE t.tweet LIKE "%%%s%%" ORDER BY t.created DESC LIMIT %d,5' , $_GET['search_word'], $start);
-  // sprint('文字%_' . 'ほげ') → %_ が数字なら%d,  %_が文字なら%s
-} else {
-  // 通常の処理(検索していない場合の全件表示の処理)
-  $sql = sprintf('SELECT t.*, m.nick_name, m.picture_path FROM `tweets` AS t LEFT JOIN `members` AS m ON t.member_id=m.member_id ORDER BY t.created DESC LIMIT %d,5' , $start);
-  // $sql = 'SELECT t.*, m.nick_name, m.picture_path FROM `tweets` t `members` m WHERE t.member_id=m.member_id';
-}
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
+// ↓sql文で取得したデータを配列化
+$haiku_ids = array();
+while($evaluation_like = $like_stmt->fetch(PDO::FETCH_ASSOC)) {
+  $haiku_ids[] = $evaluation_like['haiku_id'];
+} //        ↑ここで配列化 []を使うと、[1]-> [1,2]-> [1,2,3]...のように新しい数を配列の最後尾にいれる
 
- ?>
+// 配列化されたデータの中で最も一致するものが多いデータを表示
+print_r(array_count_values($haiku_ids));
+
+echo '<pre>';
+var_dump($haiku_ids);
+echo '</pre>';
+
+
+// あしランキング用sql文
+$sql = 'SELECT * FROM `dislikes`';
+$dislike_stmt = $dbh->prepare($sql);
+$dislike_stmt->execute();
+$evaluation_like = $dislike_stmt->fetch(PDO::FETCH_ASSOC);
+ // $data : sql文の中で「?」を使いたい時必要
+
+
+//  // 配列
+// // 値を複数入れるタンス
+// // $配列名 = array(値1, 値2, 値3);
+// $members = array('Kensuke', 'Taka', 'Saki', 'Hayato'); //定義
+// echo $members[1]; //使用
+?>
