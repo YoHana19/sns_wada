c5afeb5fbedbd06c0be289abbed393fbe3e603af
yohanashima <yohanashima@mail.com>
2017-04-26 01:03:38 +0800

    initial commit

---
+++ chat.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+  
+
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
