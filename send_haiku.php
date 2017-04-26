a9d35573051399256ed51ea8ac786a02a992fe71
yohanashima <yohanashima@mail.com>
2017-04-26 22:29:43 +0800

    基本は完了

--- send_haiku.php
+++ send_haiku.php
@@ -1,9 +1,31 @@
 <?php
 session_start();
 require('dbconnect.php');
+// var_dump($_FILES['photo_file']['name']);
 
 // ログインチェック
 if (isset($_SESSION['login_member_id'])) {
+
+  // 坊主ポイント
+  $sql = 'SELECT * FROM `members` WHERE `member_id`=?';
+  $data = array($_SESSION['login_member_id']);
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute($data);
+  $record = $stmt->fetch(PDO::FETCH_ASSOC);
+  $bozu_point = $record['bozu_points'];
+
+  // 上の句
+  if ($_POST['up_haiku'] != 5) {
+    $bozu_point += 1;
+  }
+  // 中の句
+  if ($_POST['md_haiku'] != 7) {
+    $bozu_point += 1;
+  }
+  // 下の句
+  if ($_POST['lw_haiku'] != 5) {
+    $bozu_point += 1;
+  }
 
   if ($_POST['page'] == 'chat') { // チャット
   
@@ -19,6 +41,12 @@ if (isset($_SESSION['login_member_id'])) {
                                     `created`=NOW()
                                     ';
     $data = array($_SESSION['login_member_id'],$_POST['friend_id'],$_POST['room'],$_POST['up_haiku'],$_POST['md_haiku'],$_POST['lw_haiku'],$_FILES['photo_file']['name']);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    // 坊主ポイントDB登録
+    $sql = 'UPDATE `members` SET `bozu_points`=? WHERE `member_id`=?';
+    $data = array($bozu_point, $_SESSION['login_member_id']);
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
 
@@ -39,6 +67,12 @@ if (isset($_SESSION['login_member_id'])) {
                                      `created`=NOW()
                                      ';
     $data = array($_SESSION['login_member_id'],$_POST['up_haiku'],$_POST['md_haiku'],$_POST['lw_haiku'],$_FILES['photo_file']['name'],$_POST['short_comment']);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    // 坊主ポイントDB登録
+    $sql = 'UPDATE `members` SET `bozu_points`=? WHERE `member_id`=?';
+    $data = array($bozu_point, $_SESSION['login_member_id']);
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
 
