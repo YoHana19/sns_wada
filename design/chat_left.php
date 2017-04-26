ea42d87eaa25b433735c0703062c1822b34adc05
yohanashima <yohanashima@mail.com>
2017-04-26 23:56:26 +0800

    更新

---
+++ design/chat_left.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <!-- チャット一覧 -->
+    <!-- 検索フォーム -->
+    <form action="" method="" accept-charset="utf-8" class="form-horizontal">
+      <div class="form-group search-input"  style="margin: 0;">
+        <div class="col-md-9" style="text-align: center; padding: 10px;">
+          <input type="text" name="" class="form-control" style="padding: 3px 6px; height: auto;">
+        </div>
+        <div class="col-md-3" style="text-align: center; padding: 10px 0;">
+          <input type="submit" class="btn btn-info" value="検索" style="background-color: #2ca9e1; padding: 3px 6px;">
+        </div>
+      </div>
+    </form>
+
+    <!-- kosei's 検索フォーム -->
+    <!-- <div class="search">
+      <input type="text" class="form-control input-sm" maxlength="64" placeholder="検索" />
+      <button type="submit" class="btn btn-primary btn-sm">検索</button>
+    </div> -->
+
+    <h4><b>自分情報</b></h4>
+    <div class="well_2">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object" src="http://placekitten.com/120/120">
+        </a>
+          <div class="media-body">
+            <h4 class="media-heading">・ユーザー名</h4>
+            <p class="text-right"></p>
+            <p>・自己紹介句</p>
+          </div>
+      </div>
+    </div>
+
+    <h4>友達一覧</h4>
+    <div class="well_3">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object" src="http://placekitten.com/110/110">
+        </a>
+          <div class="media-body">
+            <h4 class="media-heading">・ユーザー名</h4>
+              <p class="text-right"></p>
+              <p>・自己紹介句</p>
+          </div>
+      </div>
+    </div>
+
+    <div class="well_3">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object" src="http://placekitten.com/110/110">
+        </a>
+          <div class="media-body">
+            <h4 class="media-heading">・ユーザー名</h4>
+            <p class="text-right"></p>
+            <p>・自己紹介句</p>
+          </div>
+      </div>
+    </div>
+
+    <div class="well_3">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="media-object" src="http://placekitten.com/110/110">
+        </a>
+          <div class="media-body">
+            <h4 class="media-heading">・ユーザー名</h4>
+            <p class="text-right"></p>
+            <p>・自己紹介句</p>
+          </div>
+      </div>
+    </div>
+</body>
+</html>
