a58e798e1eced7d7e4e98664289711d4668cb2b1
yohanashima <yohanashima@mail.com>
2017-04-26 14:21:43 +0800

    微修正

--- send_haiku.php
+++ send_haiku.php
@@ -1,28 +1,48 @@
 <?php
 session_start();
 require('dbconnect.php');
-
 // var_dump($_FILES['photo_file']['name']);
 
 // ログインチェック
 if (isset($_SESSION['login_member_id'])) {
 
-  // 送られてきた俳句をDBに追加
-  $sql = 'INSERT INTO `haikus` set `haiku_id`=NULL,
-                                  `member_id`=?,
-                                  `haiku_1`=?,
-                                  `haiku_2`=?,
-                                  `haiku_3`=?,
-                                  `back_img`=?,
-                                  `short_comment`=?,
-                                  `created`=NOW()
-                                   ';
-  $data = array($_SESSION['login_member_id'],$_POST['up_haiku'],$_POST['md_haiku'],$_POST['lw_haiku'],$_FILES['photo_file']['name'],$_POST['short_comment']);
-  $stmt = $dbh->prepare($sql);
-  $stmt->execute($data);
+  if ($_POST['page'] == 'chat') { // チャット
+  
+    // 送られてきた俳句をDBに追加
+    $sql = 'INSERT INTO `chats` set `chat_id`=NULL,
+                                     `sender_id`=?,
+                                     `reciever_id`=?,
+                                     `room_id`=?,
+                                     `chat_1`=?,
+                                     `chat_2`=?,
+                                     `chat_3`=?,
+                                     `back_img`=?,
+                                     `created`=NOW()
+                                     ';
+    $data = array($_SESSION['login_member_id'],$_POST['friend_id'],$_POST['room'],$_POST['up_haiku'],$_POST['md_haiku'],$_POST['lw_haiku'],$_FILES['photo_file']['name']);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
 
+    header('Location: chat.php');
+  
+  } else { // タイムライン
+
+    // 送られてきた俳句をDBに追加
+    $sql = 'INSERT INTO `haikus` set `haiku_id`=NULL,
+                                     `member_id`=?,
+                                     `haiku_1`=?,
+                                     `haiku_2`=?,
+                                     `haiku_3`=?,
+                                     `back_img`=?,
+                                     `short_comment`=?,
+                                     `created`=NOW()
+                                     ';
+    $data = array($_SESSION['login_member_id'],$_POST['up_haiku'],$_POST['md_haiku'],$_POST['lw_haiku'],$_FILES['photo_file']['name'],$_POST['short_comment']);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    header('Location: timeline.php');
+  }
 }
-
-header('Location: timeline.php');
 exit();
 ?>
