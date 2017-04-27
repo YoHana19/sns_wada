bd633e2fc7e952dd25161aa151073f8f0fd66ff3
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-04-27 20:54:20 +0800

    更新

--- design/header.php
+++ design/header.php
@@ -4,11 +4,12 @@
 	<meta charset="utf-8">
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="../assets/css/header.css">
 </head>
 <body>
-  <nav class="navbar navbar-webmaster" style="width: 1280px; height: 40px">
+  <nav class="navbar navbar-webmaster" style="width:'auto'; height: 80px">
+    <div class=""></div>
     <div class="header-bk">
       <div class="navbar-header">
         <a class="navbar-brand" href="#">和だ</a>
@@ -20,8 +21,8 @@
         </form>
         <ul class="nav navbar-nav navbar-right">
           <li class="active"><a href="#">友達リクエスト<span class="sr-only">(current)</span></a></li>
-          <li class="active"><a href="#">個人ページ<span class="sr-only">(current)</span></a></li>
-          <li class="active"><a href="#">chat</i><span class="sr-only">(current)</span></a></li>
+          <li class="active"><a href="#">個人ページ<i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
+          <li class="active"><a href="#">chat<span class="sr-only">(current)</span></a></li>
           <li class="active"><a href="#">友達一覧<span class="sr-only">(current)</span></a></li>
           <li class="active"><a href="#">ランク・コラム<span class="sr-only">(current)</span></a></li>
           <li id="logout" class=""><a href="#logoutModal" data-toggle="modal" data-target="#logoutModal">ログアウト</a></li>
