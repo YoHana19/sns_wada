75934f30b7eba979e791900c787363128348aa8d
yohanashima <yohanashima@mail.com>
2017-04-27 12:38:37 +0800

    更新

--- design/friends_left.php
+++ design/friends_left.php
@@ -20,7 +20,18 @@
     </div>
   </form>
 
-  <!-- チャット一覧 -->
+  <!-- 個人プロフ -->
+  <div class="media" style="position: relative; margin-top: 7px">
+    <a class="pull-left chat-photo" href="#">
+      <img class="media-object" src="http://placekitten.com/110/110" style="width: 55px; height: 55px; border-radius: 50%">
+    </a>
+    <div class="media-body chat-body">
+      <p class="media-heading chat-nickname">たかさん</p>
+      <p class="chat-intro">あああああ　あああああああ　あああああ</p>
+    </div>
+  </div>
+
+  <!-- 友達一覧 -->
   <div class="well_3 chat-room">
     <?php for ($i=0; $i < 15; $i++) { ?>
       <div class="media" style="position: relative; margin-top: 7px">
