6bf7788d8783aa5dba4e9aa9adeac3c1bf3906e0
KOSEI INOUE <816kosei2017@gmail.com>
2017-04-27 14:02:06 +0800

    ランキング上位3件表示 句、プロフ画像、名前、よし数を記載→句の詳細ページ、個人ページに遷移できる: 未

--- runking.php
+++ runking.php
@@ -5,23 +5,23 @@ require('dbconnect.php');
 
 // よしランキング句用sql文
 $sql = 'SELECT * FROM `likes`';
-$like_stmt = $dbh->prepare($sql);
-$like_stmt->execute();
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+// $data : sql文の中で「?」を使いたい時必要
 
 // ↓sql文で取得したデータを配列化
 $haiku_ids = array();
-while($evaluation_like = $like_stmt->fetch(PDO::FETCH_ASSOC)) {
-  $haiku_ids[] = $evaluation_like['haiku_id'];
-} //        ↑ここで配列化 []を使うと、[1]-> [1,2]-> [1,2,3]...のように新しい数を配列の最後尾にいれる
+while($like_haiku = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $haiku_ids[] = $like_haiku['haiku_id'];
+}       // ↑ここで配列化 []を使うと、[1]-> [1,2]-> [1,2,3]...のように新しい数を配列の最後尾にいれる
 
-//↓重複する値を一致させ、非破壊的要素のため変数に代入
+//↓重複する値を一致させ、非破壊的関数のため変数に代入
 array_count_values($haiku_ids);
 $pure_ids = array_count_values($haiku_ids);
 
 // 一致したものが最も多いもの上位3件文取得
-// ksortでキーを昇順に並び替える
-ksort($pure_ids);
-print_r($pure_ids);
+// arsortでキーを昇順に並び替える
+arsort($pure_ids);
 $key = key($pure_ids);
 
 echo $key;
@@ -31,76 +31,51 @@ $key1 = key($pure_ids);
 echo $key1;
 next($pure_ids);
 $key2 = key($pure_ids);
-echo $key2;
-
-// sql文で上位3件分データ取得
-$sql = 'SELECT * FROM `likes` WHERE `haiku_id`';
-$good_stmt_1 = $dbh->prepare($sql);
-$good_stmt_1->execute();
-$evaluation_like_1 = $good_stmt_1->fetch(PDO::FETCH_ASSOC);
-// echo $evaluation_like_1['haiku_id'];
-key($evaluation_like_1);
-// var_dump($evaluation_like_1);
+echo $key2 . '<br>';
 
 
-
-// よしランキング俳人用sql分
+// よしランキング歌人用sql分
 $sql = 'SELECT * FROM `likes`';
-$like_member_stmt = $dbh->prepare($sql);
-$like_member_stmt->execute();
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
 
-$haiku_g_member_ids = array();
-while($evaluation_like_member = $like_member_stmt->fetch(PDO::FETCH_ASSOC)) {
-  $haiku_g_member_ids[] = $evaluation_like_member['haiku_member_id'];
+$haiku_member_ids = array();
+while($like_member = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $haiku_member_ids[] = $like_member['haiku_member_id'];
 }
 
-array_count_values($haiku_g_member_ids);
-$pure_g_member_ids = array_count_values($haiku_g_member_ids);
-
-ksort($pure_g_member_ids);
-print_r($pure_g_member_ids);
-$good_member_key = key($pure_g_member_ids);
-
-echo $good_member_key;
-next($pure_g_member_ids);
-$good_member_key_1 = key($pure_g_member_ids);
-
-echo $good_member_key_1;
-next($pure_g_member_ids);
-$good_member_key_2 = key($pure_g_member_ids);
-echo $good_member_key_2;
-
-// sql文で上位3件分データ取得
-$sql = 'SELECT * FROM `likes` WHERE `haiku_member_id`';
-$good_member_stmt_1 = $dbh->prepare($sql);
-$good_member_stmt_1->execute();
-$evaluation_member_like_1 = $good_member_stmt_1->fetch(PDO::FETCH_ASSOC);
-// echo $evaluation_like_1['haiku_id'];
-key($evaluation_member_like_1);
-// var_dump($evaluation_like_1);
+array_count_values($haiku_member_ids);
+$pure_member_ids = array_count_values($haiku_member_ids);
 
 
+arsort($pure_member_ids);
+$member_key = key($pure_member_ids);
 
-// あしランキング用sql文
+echo $member_key;
+next($pure_member_ids);
+$member_key_1 = key($pure_member_ids);
+
+echo $member_key_1;
+next($pure_member_ids);
+$member_key_2 = key($pure_member_ids);
+echo $member_key_2 . '<br>';
+
+
+// あしランキング句用sql文
 $sql = 'SELECT * FROM `dislikes`';
-$dislike_stmt = $dbh->prepare($sql);
-$dislike_stmt->execute();
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
 
-// ↓sql文で取得したデータを配列化
 $bad_haiku_ids = array();
-while($evaluation_dislike = $dislike_stmt->fetch(PDO::FETCH_ASSOC)) {
-  $bad_haiku_ids[] = $evaluation_dislike['haiku_id'];
-} //        ↑ここで配列化 []を使うと、[1]-> [1,2]-> [1,2,3]...のように新しい数を配列の最後尾にいれる
+while($dislike_haiku = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $bad_haiku_ids[] = $dislike_haiku['haiku_id'];
+}
 
-//↓重複する値を一致させ、非破壊的要素のため変数に代入
 array_count_values($bad_haiku_ids);
 $bad_pure_ids = array_count_values($bad_haiku_ids);
 
-// 一致したものが最も多いもの上位3件文取得
-// ksortでキーを昇順に並び替える
-ksort($bad_pure_ids);
+arsort($bad_pure_ids);
 
-print_r($bad_pure_ids);
 $bad_key = key($bad_pure_ids);
 echo $bad_key;
 
@@ -110,15 +85,32 @@ echo $bad_key_1;
 
 next($bad_pure_ids);
 $bad_key_2 = key($bad_pure_ids);
-echo $bad_key_2;
+echo $bad_key_2 . '<br>';
 
-// sql文で上位3件分データ取得
-$sql = 'SELECT * FROM `dislikes` WHERE `haiku_id`';
-$bad_stmt_1 = $dbh->prepare($sql);
-$bad_stmt_1->execute();
-$evaluation_dislike_1 = $bad_stmt_1->fetch(PDO::FETCH_ASSOC);
-// echo $evaluation_like_1['haiku_id'];
-key($evaluation_dislike_1);
-// var_dump($evaluation_like_1);
-// $data : sql文の中で「?」を使いたい時必要
+
+// あしランキング歌人用sql分
+$sql = 'SELECT * FROM `dislikes`';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$bad_member_ids = array();
+while($dislike_member = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  $bad_member_ids[] = $dislike_member['haiku_member_id'];
+}
+
+array_count_values($bad_member_ids);
+$bad_member_ids = array_count_values($bad_member_ids);
+
+arsort($bad_member_ids);
+$member_key = key($bad_member_ids);
+
+echo $member_key;
+next($bad_member_ids);
+$member_key_1 = key($bad_member_ids);
+
+echo $member_key_1;
+next($bad_member_ids);
+$member_key_2 = key($bad_member_ids);
+echo $member_key_2 . '<br>';
+
 ?>
