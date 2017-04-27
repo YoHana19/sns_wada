3513a401bedb4d826a1340a1d262fc91054a680a
yohanashima <yohanashima@mail.com>
2017-04-27 19:08:51 +0800

    微修正

--- design/header.php
+++ design/header.php
@@ -3,12 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
-	<link rel="stylesheet" type="text/css" href="../assets/css/header.css">
 </head>
 <body>
-  <nav class="navbar navbar-webmaster" style="width: 1280px; height: 40px">
+  <nav class="navbar navbar-webmaster" style="height: 40px">
     <div class="header-bk">
       <div class="navbar-header">
         <a class="navbar-brand" href="#">和だ</a>
