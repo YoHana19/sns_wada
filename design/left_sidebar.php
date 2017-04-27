9450a39a0e80e624f3856f2fe01679b00bf795e6
yohanashima <yohanashima@mail.com>
2017-04-27 15:49:31 +0800

    左サイドバー完成

--- design/left_sidebar.php
+++ design/left_sidebar.php
@@ -7,33 +7,35 @@
 <body>
 
   <!-- 簡易個人プロフ -->
-  <img src="../assets/images/saito.jpeg" id="photo">
-  <h3>齋藤 由佳</h3>
-  <h5>おねぇさん トレンディだね 齋藤さん</h5>
-  <div class="clearfix"></div>
+  <div class="left-proph">
+    <img src="../assets/images/saito.jpeg" id="photo">
+    <h3>齋藤 由佳</h3>
+    <span class="intro-text-3">お<br>ね<br>ぇ<br>さ<br>ん</span>
+    <span class="intro-text-2">ト<br>レ<br>ン<br>デ<br>ィ<br>だ<br>ね</span>
+    <span class="intro-text-1">齋<br>藤<br>さ<br>ん</span>
+  </div>
+    <div class="clearfix"></div>
 
-  <!-- 友達表示 -->
-  <div class="panel panel-default">  <!-- 「Friends」の一番上の枠 -->
-    <div class="panel-heading c-list">  <!-- 「Friends」の一番上の枠のパネル -->
-      <span class="title">お友達</span>
+  <div class="friends-display">
+    <!-- タイトル表示 -->
+    <div class="friends-title">
+      <span class="title">お仲間</span>
     </div>
 
     <!-- 直近連絡とった友達順に10件表示 -->
-    <div class="well_3 chat-room">
+    <div class="well_3">
       <?php for ($i=0; $i < 15; $i++) { ?>
         <div class="media" style="position: relative; margin-top: 7px">
-          <a class="pull-left chat-photo" href="#">
+          <a class="pull-left left-photo" href="#">
             <img class="media-object" src="http://placekitten.com/110/110" style="width: 55px; height: 55px; border-radius: 50%">
           </a>
-          <div class="media-body chat-body">
-            <p class="media-heading chat-nickname">たかさん</p>
-            <p class="chat-intro">あああああ　あああああああ　あああああ</p>
-          </div>
+        <div class="media-body left-display">
+          <span class="media-heading left-nickname">たかさん</span>
+          <p class="left-intro">あああああ　あああああああ　あああああ</p>
         </div>
+      </div>
       <?php } ?>
     </div>
-    
   </div>
-
 </body>
 </html>
