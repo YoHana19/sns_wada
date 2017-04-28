6dcf8a599c24d601c9244bacdbad661c5b139cc8
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-04-28 19:06:12 +0800

    微調整

--- design/header.php
+++ design/header.php
@@ -12,20 +12,7 @@
         <a class="navbar-brand" href="#">和だ</a>
       </div>
       <div class="collapse navbar-collapse" id="navbar">
-        <form class="navbar-form navbar-right search-form form-horizontal" role="search">
-          <!-- 検索フォーム -->
-          <div id="custom-search-input">
-            <div class="input-group">
-              <input type="text" class="search-query form-control" placeholder="search">
-              <span class="input-group-btn">
-                <button class="btn btn-danger" type="button">
-                  <span class="glyphicon glyphicon-search" style="color: #dcdddd"></span>
-                </button>
-              </span>
-            </div>
-          </div>
-          <input type="button" class="btn btn-info haiku-input" value="詠む">
-        </form>
+        
         <ul class="nav navbar-nav navbar-right">
           <li class="active"><a href="#">友達リクエスト<span class="sr-only">(current)</span></a></li>
           <li class="active"><a href="#">個人ページ<i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
@@ -33,6 +20,23 @@
           <li class="active"><a href="#">友達一覧<span class="sr-only">(current)</span></a></li>
           <li class="active"><a href="#">ランク・コラム<span class="sr-only">(current)</span></a></li>
           <li id="logout" class=""><a href="#logoutModal" data-toggle="modal" data-target="#logoutModal">ログアウト</a></li>
+          <li id="logout" class=""><a href="#logoutModal" data-toggle="modal" data-target="#logoutModal"><i class="fa fa-caret-square-o-up" aria-hidden="true"></i></a></li>
+
+            <form class="navbar-form navbar-right search-form form-horizontal" role="search">
+              <!-- 検索フォーム -->
+              <div id="custom-search-input">
+                <div class="input-group">
+                  <input type="text" class="search-query form-control" placeholder="search">
+                    <span class="input-group-btn">
+                      <button class="btn btn-danger" type="button">
+                        <span class="glyphicon glyphicon-search" style="color: #dcdddd"></span>
+                      </button>
+                    </span>
+                </div>
+                <input type="button" class="btn btn-info haiku-input" value="詠む">
+              </div>
+              
+            </form>
         </ul>
       </div>
     </div>
