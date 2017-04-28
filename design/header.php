445ef74e8a3f8da7aa65f7fb97e141659476d0ae
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-04-28 13:16:26 +0800

    更新

--- design/header.php
+++ design/header.php
@@ -12,8 +12,18 @@
         <a class="navbar-brand" href="#">和だ</a>
       </div>
       <div class="collapse navbar-collapse" id="navbar">
-        <form class="navbar-form navbar-right search-form" role="search">
-          <input type="text" class="form-control" placeholder="Search">
+        <form class="navbar-form navbar-right search-form form-horizontal" role="search">
+          <!-- 検索フォーム -->
+          <div id="custom-search-input">
+            <div class="input-group">
+              <input type="text" class="search-query form-control" placeholder="search">
+              <span class="input-group-btn">
+                <button class="btn btn-danger" type="button">
+                  <span class="glyphicon glyphicon-search" style="color: #dcdddd"></span>
+                </button>
+              </span>
+            </div>
+          </div>
           <input type="button" class="btn btn-info haiku-input" value="詠む">
         </form>
         <ul class="nav navbar-nav navbar-right">
