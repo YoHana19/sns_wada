8cbbe5495af5aa991ed1bdaa8fe1d782afdbcea9
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-05-01 18:47:19 +0800

    完成

--- design/footer.php
+++ design/footer.php
@@ -8,15 +8,18 @@
   <footer class="footer">
     <div class="container" style="width: 1279px; height: 40px">
       <div class="row">
-        <p>© 2017 SNS_wada, All rights reserved.</
+        <div class="footer1">© 2017 SNS_wada, All rights reserved.
         <!-- <ul class="social-icon animate pull-right"> -->
-        <div class="row">
+        <!-- <div class="row"> -->
         <!-- <div class="col-md-3"></div> -->
-          <a href="#" title="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-          <a href="#" title="twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-facebook-official fa-1x" aria-hidden="true" style="color: #3b5998"></i></a>
+          <a href="#"><i class="fa fa-twitter-square fa-1x" aria-hidden="true" style="color: #00a1e9"></i></a>
+          <!-- <a href="#" title="facebook" target="_blank" style="color: #3b5998"><i class="fa fa-facebook"></i></a> -->
+          <!-- <a href="#" title="twitter" target="_blank" style="color: #00a1e9"><i class="fa fa-twitter"></i></a> -->
         </div>
+        <!-- </div> -->
       </div>
     </div>
-
+  </footer>
 </body>
 </html>
