1c3a488b1d21a4efe60f579b8d96d1e60dff9f4d
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-05-02 20:45:43 +0800

    微調整

--- footer.php
+++ footer.php
@@ -13,10 +13,10 @@
         <!-- <ul class="social-icon animate pull-right"> -->
         <!-- <div class="row"> -->
         <!-- <div class="col-md-3"></div> -->
-          <a href="#"><i class="fa fa-facebook-official fa-1x" aria-hidden="true" style="color: #3b5998"></i></a>
-          <a href="#"><i class="fa fa-twitter-square fa-1x" aria-hidden="true" style="color: #00a1e9"></i></a>
-          <!-- <a href="#" title="facebook" target="_blank" style="color: #3b5998"><i class="fa fa-facebook"></i></a> -->
-          <!-- <a href="#" title="twitter" target="_blank" style="color: #00a1e9"><i class="fa fa-twitter"></i></a> -->
+          <!-- <a href="#"><i class="fa fa-facebook-official fa-2x" aria-hidden="true" style="color: #3b5998"></i></a> -->
+          <!-- <a href="#"><i class="fa fa-twitter-square fa-1x" aria-hidden="true" style="color: #00a1e9"></i></a> -->
+          <a href="#" title="facebook" target="_blank" style="color: #3b5998"><i class="fa fa-facebook"></i></a>
+          <a href="#" title="twitter" target="_blank" style="color: #00a1e9"><i class="fa fa-twitter"></i></a>
         </div>
         <!-- </div> -->
       </div>
