5cf76c19b8d1f198bb8d46ef4a4163ade44abae7
takahiro wada <takahiro@takahiro-MacBook-Pro.local>
2017-05-02 12:22:57 +0800

    微調整

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body background="../assets/images/back.jpg" bgproperties="fixd">
+  <footer class="footer">
+    <div class="container" style="width: 1279px; height: 40px">
+      <div class="row"> 
+        <div class="footer1">© 2017 SNS_wada, All rights reserved.
+        <!-- <ul class="social-icon animate pull-right"> -->
+        <!-- <div class="row"> -->
+        <!-- <div class="col-md-3"></div> -->
+          <a href="#"><i class="fa fa-facebook-official fa-1x" aria-hidden="true" style="color: #3b5998"></i></a>
+          <a href="#"><i class="fa fa-twitter-square fa-1x" aria-hidden="true" style="color: #00a1e9"></i></a>
+          <!-- <a href="#" title="facebook" target="_blank" style="color: #3b5998"><i class="fa fa-facebook"></i></a> -->
+          <!-- <a href="#" title="twitter" target="_blank" style="color: #00a1e9"><i class="fa fa-twitter"></i></a> -->
+        </div>
+        <!-- </div> -->
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
