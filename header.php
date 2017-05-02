166a4d20ba339450903f98360b0d177a153ecf6a
yohanashima <yohanashima@mail.com>
2017-05-02 20:15:32 +0800

    更新

--- header.php
+++ header.php
@@ -39,3 +39,55 @@ require('dbconnect.php');
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <nav class="navbar navbar-webmaster" style="width:'auto'; height: 80px">
+    <div class=""></div>
+    <div class="header-bk">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="timeline.php">和だ</a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav navbar-right">
+          <!-- 友達リクエスト -->
+          <li class="active click"><a href="#"><i class="fa fa-user-plus fa-2x" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
+          <!-- プロフページ -->
+          <li class="active click"><a href="profile.php"><i class="fa fa-user fa-2x" aria-hidden="true"></i></i><i class="fa fa-user-circle-o" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
+          <!-- チャットページ -->
+          <li class="active click"><a href="chat.php"><i class="fa fa-comments fa-2x" aria-hidden="true"></i></i><span class="sr-only">(current)</span></a></li>
+          <!-- 友達一覧ページ -->
+          <li class="active click"><a href="friends.php"><i class="fa fa-users fa-2x" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
+          <!-- ランキングページ -->
+          <li class="active click"><a href="ranking.php"><i class="fa fa-sort-numeric-asc fa-2x" aria-hidden="true"></i><span class="sr-only">(current)</span></a></li>
+          <!-- ログアウト -->
+          <li id="logout" class=""><a href="logout.php" data-toggle="modal" data-target="#logoutModal"><i class="fa fa-sign-out fa-2x" aria-hidden="true"></i></a></li></a></li>
+
+          <form class="navbar-form navbar-right search-form form-horizontal" role="search">
+            <!-- 検索フォーム -->
+            <div id="custom-search-input">
+              <div class="input-group">
+                <input type="text" class="search-query form-control" placeholder="search">
+                  <span class="input-group-btn">
+                    <button class="btn btn-danger" type="button">
+                      <span class="glyphicon glyphicon-search" style="color: #dcdddd"></span>
+                    </button>
+                  </span>
+              </div>
+              <input type="button" class="btn btn-info haiku-input" value="詠む">
+            </div>
+          </form>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </nav>
+
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
