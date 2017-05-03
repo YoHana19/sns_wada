f948f4c0e2ca6e7aa2974f5c919482e0e6bd3057
yohanashima <yohanashima@mail.com>
2017-05-03 19:10:18 +0800

    for こうせい

--- test.php
+++ test.php
@@ -2,8 +2,12 @@
 session_start();
 require('dbconnect.php');
 
-$sql = 'SELECT * FROM `rooms`';
+$sql = 'SELECT * FROM `rooms` WHERE `member_id_1`=1';
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
+$records = array();
+while ($record = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      $records[] = $record;
+}
 ?>
